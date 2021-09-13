307fc4812286f46ad492dfa968731b2ea6c68f06
MackorLab <micoght@gmail.com>
2021-09-14 00:43:10 +0300

    Updates

---
+++ precache-manifest.3a93cb00124830a609932c91657bce16.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a285f003d8e882a1fc11cd9c42c3a21d",
+    "url": "/planirovshik/index.html"
+  },
+  {
+    "revision": "9bb9dfffba27d31f9a6f",
+    "url": "/planirovshik/static/css/main.6941b91f.chunk.css"
+  },
+  {
+    "revision": "bde50ce89fff438cd41d",
+    "url": "/planirovshik/static/js/2.0be88deb.chunk.js"
+  },
+  {
+    "revision": "9bb9dfffba27d31f9a6f",
+    "url": "/planirovshik/static/js/main.7abfb5f8.chunk.js"
+  },
+  {
+    "revision": "830b07a8f6f3dee37b2c",
+    "url": "/planirovshik/static/js/runtime~main.fc62cad4.js"
+  },
+  {
+    "revision": "6cb6e5fc57e9600d8f9f7cdfaa120d3d",
+    "url": "/planirovshik/static/media/add.6cb6e5fc.svg"
+  },
+  {
+    "revision": "8acdee3dfd048bedf6c9123b22a8b22d",
+    "url": "/planirovshik/static/media/background.8acdee3d.jpg"
+  },
+  {
+    "revision": "5fe0db661a8f134c8ff7cb418373c469",
+    "url": "/planirovshik/static/media/clear.5fe0db66.svg"
+  }
+]);
