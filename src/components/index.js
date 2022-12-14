233528cd1fd24f822b89141d46d0458a6c6d669e
MackorLab <76862637+MackorLab@users.noreply.github.com>
2022-12-14 13:55:13 +0700

    Delete src/components directory

--- src/components/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Card } from "./Card";
-export { default as Column } from "./Column";
-export { default as Button } from "./Button";
-export { default as AddForm } from "./AddForm";
